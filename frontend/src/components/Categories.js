4884288b416589f9fee0d75692844252949a688f
Reio Kruusement <reiokr@gmail.com>
2026-06-02 00:16:34 +0300

    modified:   .gitignore 	modified:   frontend/src/components/Categories.js 	modified:   frontend/src/css/Addform.css 	new file:   frontend/src/css/Addform.css.map 	modified:   frontend/src/css/App.css 	new file:   frontend/src/css/App.css.map 	modified:   frontend/src/css/Bm.css 	new file:   frontend/src/css/Bm.css.map 	modified:   frontend/src/css/Categories.css 	new file:   frontend/src/css/Categories.css.map 	modified:   frontend/src/css/ControlBar.css 	new file:   frontend/src/css/ControlBar.css.map 	modified:   frontend/src/css/Description.css 	new file:   frontend/src/css/Description.css.map 	modified:   frontend/src/css/Home.css 	new file:   frontend/src/css/Home.css.map 	modified:   frontend/src/css/Loader.css 	new file:   frontend/src/css/Loader.css.map 	modified:   frontend/src/css/Login.css 	new file:   frontend/src/css/Login.css.map 	modified:   frontend/src/css/Navbar.css 	new file:   frontend/src/css/Navbar.css.map 	modified:   frontend/src/css/User.css 	new file:   frontend/src/css/User.css.map 	modified:   frontend/src/css/YTvideoPlayer.css 	new file:   frontend/src/css/YTvideoPlayer.css.map 	modified:   frontend/src/css/signup.css 	new file:   frontend/src/css/signup.css.map 	modified:   frontend/src/scss/Addform.scss 	modified:   frontend/src/scss/Categories.scss 	modified:   frontend/src/scss/Description.scss 	modified:   frontend/src/scss/_config.scss

--- frontend/src/components/Categories.js
+++ frontend/src/components/Categories.js
@@ -1,139 +1,122 @@
-import { useEffect } from 'react';
-import '../css/Categories.css';
-import BmContainer from './BmContainer';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { updateCategories } from '../actions/authActions';
-import { activeCategory } from '../actions/bmActions';
-import { clearErrors } from '../actions/errorActions';
-import { BsFolderPlus, BsFolderMinus } from 'react-icons/bs';
-import AddCategoryModal from './AddCategoryModal';
-import ControlBar from './ControlBar';
-import { useStateContext } from '../context/ContextProvider';
+import { useEffect } from "react";
+import "../css/Categories.css";
+import BmContainer from "./BmContainer";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { updateCategories } from "../actions/authActions";
+import { activeCategory } from "../actions/bmActions";
+import { clearErrors } from "../actions/errorActions";
+import { BsFolderPlus, BsFolderMinus } from "react-icons/bs";
+import AddCategoryModal from "./AddCategoryModal";
+import ControlBar from "./ControlBar";
+import { useStateContext } from "../context/ContextProvider";
 
 const Categories = ({
-    data,
-    auth,
-    video,
-    updateCategories,
-    activeCategory,
-    clearErrors,
+  data,
+  auth,
+  video,
+  updateCategories,
+  activeCategory,
+  clearErrors,
 }) => {
-    const {
-        addCategoryModal,
-        setAddCategoryModal,
-        isEmptyCategory,
-        categories,
-    } = useStateContext();
-    const toggleModal = () => {
-        setAddCategoryModal((prev) => !prev);
-    };
-    const handleDelCategory = () => {
-        const currentCategories = auth.user.categories;
-        const updatedCategories = currentCategories.filter(
-            (c) => c !== data.activeCategory
-        );
-        if (data?.bm?.length === 0) updateCategories(updatedCategories,data.activeCategory);
-    };
-    const handleChangeCategory = (category) => {
-        activeCategory(category);
-        clearErrors();
-    };
+  const { addCategoryModal, setAddCategoryModal, isEmptyCategory, categories } =
+    useStateContext();
+  const toggleModal = () => {
+    setAddCategoryModal((prev) => !prev);
+  };
+  const handleDelCategory = () => {
+    const currentCategories = auth.user.categories;
+    const updatedCategories = currentCategories.filter(
+      (c) => c !== data.activeCategory,
+    );
+    if (data?.bm?.length === 0)
+      updateCategories(updatedCategories, data.activeCategory);
+  };
+  const handleChangeCategory = (category) => {
+    activeCategory(category);
+    clearErrors();
+  };
 
-    useEffect(() => {
-        if (auth?.isAuthenticated && data?.activeCategory) {
-            if (!auth.user.categories.includes(data.activeCategory)) {
-                activeCategory(auth.user.categories[0]);
-            }
-        }
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [auth?.user?.categories]);
-
-    if (auth.isAuthenticated) {
-        return (
-            <>
-                <div className="header-buttons">
-                    <div className="container">
-                        <div className="tab-buttons">
-                            {!video.video && data && (
-                                <div className="btn-container">
-                                    {categories.map((category, i) => {
-                                        return (
-                                            <button
-                                                className={`tab-btn ${
-                                                    data?.activeCategory ===
-                                                    category
-                                                        ? 'active-btn'
-                                                        : ''
-                                                }`}
-                                                key={i}
-                                                onClick={() =>
-                                                    handleChangeCategory(
-                                                        category
-                                                    )
-                                                }
-                                            >
-                                                {category}
-                                            </button>
-                                        );
-                                    })}
-                                </div>
-                            )}
-                            <ControlBar />
-                            {!video.video && (
-                                <div className="cat-buttons">
-                                    {isEmptyCategory && data.isEmptyCategory && (
-                                        <div
-                                            className="del-cat"
-                                            onClick={handleDelCategory}
-                                        >
-                                            {isEmptyCategory &&
-                                                data?.bm?.length === 0 && (
-                                                    <BsFolderMinus className="del-cat-icon" />
-                                                )}
-                                        </div>
-                                    )}
-                                    <div
-                                        className="add-cat"
-                                        onClick={toggleModal}
-                                    >
-                                        <BsFolderPlus className="add-cat-icon" />
-                                    </div>
-                                </div>
-                            )}
-                        </div>
-                        {addCategoryModal && (
-                            <AddCategoryModal
-                                toggleModal={toggleModal}
-                                addCategoryModal={addCategoryModal}
-                            />
-                        )}
-                    </div>
-                </div>
-                {auth.isAuthenticated && <BmContainer />}
-            </>
-        );
-    } else {
-        return null;
+  useEffect(() => {
+    if (auth?.isAuthenticated && data?.activeCategory) {
+      if (!auth.user.categories.includes(data.activeCategory)) {
+        activeCategory(auth.user.categories[0]);
+      }
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [auth?.user?.categories]);
+
+  if (auth.isAuthenticated) {
+    return (
+      <>
+        <div className="header-buttons">
+          <div className="container">
+            <div className="tab-buttons">
+              {!video.video && data && (
+                <div className="btn-container">
+                  {categories.map((category, i) => {
+                    return (
+                      <button
+                        className={`tab-btn ${
+                          data?.activeCategory === category ? "active-btn" : ""
+                        }`}
+                        key={i}
+                        onClick={() => handleChangeCategory(category)}
+                      >
+                        {category}
+                      </button>
+                    );
+                  })}
+                  <div className="add-cat" onClick={toggleModal}>
+                    <BsFolderPlus className="add-cat-icon" />
+                  </div>
+                </div>
+              )}
+              <ControlBar />
+              {!video.video && (
+                <div className="cat-buttons">
+                  {isEmptyCategory && data.isEmptyCategory && (
+                    <div className="del-cat" onClick={handleDelCategory}>
+                      {isEmptyCategory && data?.bm?.length === 0 && (
+                        <BsFolderMinus className="del-cat-icon" />
+                      )}
+                    </div>
+                  )}
+                </div>
+              )}
+            </div>
+            {addCategoryModal && (
+              <AddCategoryModal
+                toggleModal={toggleModal}
+                addCategoryModal={addCategoryModal}
+              />
+            )}
+          </div>
+        </div>
+        {auth.isAuthenticated && <BmContainer />}
+      </>
+    );
+  } else {
+    return null;
+  }
 };
 
 const mapStateToProps = (state) => ({
-    data: state.bm,
-    auth: state.auth,
-    video: state.ytv,
+  data: state.bm,
+  auth: state.auth,
+  video: state.ytv,
 });
 
 Categories.propTypes = {
-    data: PropTypes.object.isRequired,
-    auth: PropTypes.object.isRequired,
-    video: PropTypes.object.isRequired,
-    updateCategories: PropTypes.func.isRequired,
-    activeCategory: PropTypes.func.isRequired,
+  data: PropTypes.object.isRequired,
+  auth: PropTypes.object.isRequired,
+  video: PropTypes.object.isRequired,
+  updateCategories: PropTypes.func.isRequired,
+  activeCategory: PropTypes.func.isRequired,
 };
 
 export default connect(mapStateToProps, {
-    updateCategories,
-    activeCategory,
-    clearErrors,
+  updateCategories,
+  activeCategory,
+  clearErrors,
 })(Categories);
